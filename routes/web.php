316c0fc2ff10fabeed3d8eafbcdb4c4e52fc94b2
Aakash <aakash.patel@kgn-software.com>
2024-10-18 12:38:50 +0530

    set url
    set url

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/create_lotry_step1', function () {
+    return view('create_lotry_step1');
+});
+Route::get('/create_lotry_step2', function () {
+    return view('create_lotry_step2');
+});
+Route::get('/fix_price_setting', function () {
+    return view('fix_price_setting');
+});
 
 Auth::routes();
 
