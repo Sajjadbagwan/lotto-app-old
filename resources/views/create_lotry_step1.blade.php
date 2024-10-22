1d08eedcb3fcaf342740013ad9972bc98c8eb2f0
vishalpatelkgn <vishalkumar.patel@kgn-software.com>
2024-10-22 18:41:48 +0530

    lotry step-1 form css and page  update

--- resources/views/create_lotry_step1.blade.php
+++ resources/views/create_lotry_step1.blade.php
@@ -7,10 +7,89 @@
 <!--start content section-->
 @section('content')
 
-<h1>test</h1>
 
+<section class="lotry-form-main-wrap">
+    <div class="container">
+        <h2>Create New Lottery</h2>
+        <div class="">
+            <form action="">
+                <div class="customFormWrap inlineField">
+                    <label class="customFormLabel" for="userfname">Type</label>
+                    <select name="" id="type" class="form-select">
+                        <option value="United Kingdom">United Kingdom</option>
+                        <option value="United Kingdom">United Kingdom</option>
+                        <option value="United Kingdom">United Kingdom</option>
+                    </select>
+                </div>
 
+                <div class="customFormWrap inlineField">
+                    <label class="customFormLabel" for="lotteryname">Lottery Name</label>
+                    <input type="text" id="lotteryname" name="" value="" class="form-control">
+                </div>
 
+                <div class="customFormWrap inlineField">
+                    <label class="customFormLabel" for="">Description</label>
+                    <textarea name="" id=""
+                        class="form-control">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt</textarea>
+                </div>
+
+                <div class="customFormWrap inlineField">
+                    <label class="customFormLabel" for="">Default Entry Fee</label>
+                    <input type="number" id="" name="" value="" class="form-control">
+                </div>
+
+                <div class="customFormWrap inlineField">
+                    <label class="customFormLabel" for="">Repetion</label>
+                    <select name="" id="type" class="form-select">
+                        <option value="United Kingdom">one-off</option>
+                        <option value="United Kingdom">Weekly</option>
+                        <option value="United Kingdom">etc</option>
+                    </select>
+                </div>
+
+                <div class="customFormWrap inlineField">
+                    <label for="" class="customFormLabel">First Draw</label>
+                    <input type="date" id="" name="">
+                </div>
+
+                <div class="customFormWrap inlineField">
+                    <label class="customFormLabel" for="">First Draw's ID</label>
+                    <input type="text" id="" name="" value="" class="form-control">
+                </div>
+                <h2>Prize Award Style</h2>
+                <div class="customFormWrap inlineField">
+                    <div class="radio-button-row">
+                        <div class="radio-row">
+                            <input type="radio" id="radio1" name="radio-button" value="radio1">
+                            <label for="radio1">Guaranteed winners-system will select a fixed number of winners.</label>
+                        </div>
+                        <div class="radio-row">
+                            <input type="radio" id="radio2" name="radio-button" value="radio2">
+                            <label for="radio2">Chance of winning-winning combination detemines winners based on matching
+                                the
+                                combination.</label>
+                        </div>
+                        <div class="radio-sub-row">
+                            <div class="radio-row">
+                                <input type="radio" id="radio3" name="radio-button" value="radio3">
+                                <label for="radio3">Automatic System RNG</label>
+                            </div>
+                            <div class="radio-row">
+                                <input type="radio" id="radio4" name="radio-button" value="radio4">
+                                <label for="radio4"> Manual Combination Entry</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="button-group-row">
+                    <button type="button" class="allBtn dark">Back</button>
+                    <button type="button" class="allBtn dark">Next</button>
+                </div>
+            </form>
+
+        </div>
+    </div>
+</section>
 
 
 @endsection
